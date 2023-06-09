21254c9dd30870f04c07ca55f23fa42bc86ec551
Joshua Seabrook <joshua.d.seabrook@gmail.com>
2023-06-09 22:15:29 +0100

    Home page added

---
+++ components/Layout/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import LogoVPN from "../../public/assets/Logo.svg";
+import Facebook from "../../public/assets/Icon/facebook.svg";
+import Twitter from "../../public/assets/Icon/twitter.svg";
+import Instagram from "../../public/assets/Icon/instagram.svg";
+const Footer = () => {
+  return (
+    <div className="bg-white-300 pt-44 pb-24">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
+          <h1>Affordable<strong>Space</strong></h1>
+          <br/>
+          <p className="mb-4">
+            <strong className="font-medium">AffordableSpace</strong> is a small family-run lettings business.
+          </p>
+          <p className="text-gray-400">©{new Date().getFullYear()} - AffordableSpace</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Pricing.js
@@ -0,0 +1,39 @@
+import React, { useMemo } from "react";
+import Image from "next/image";
+import Testimoni from "./Testimoni";
+import ButtonPrimary from "./misc/ButtonPrimary";
+import ButtonOutline from "./misc/ButtonOutline.";
+import Maps from "../public/assets/HugeGlobal.svg";
+import { motion } from "framer-motion";
+import getScrollAnimation from "../utils/getScrollAnimation";
+import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+
+const Pricing = () => {
+  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+
+  return (
+    <div
+      id="pricing"
+    >
+          <ScrollAnimationWrapper className="relative w-full mt-16">
+            <motion.div variants={scrollAnimation} custom={{duration: 3}}>
+              <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
+                <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
+                  <h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
+                    Only £96 per week 
+                  </h5>
+                  <p>Reserve your place while availability lasts.</p>
+                </div>
+                <ButtonPrimary>View Rooms</ButtonPrimary>
+              </div>
+              <div
+                className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
+                style={{ filter: "blur(114px)" }}
+                ></div>
+            </motion.div>
+          </ScrollAnimationWrapper>
+    </div>
+  );
+};
+
+export default Pricing;
